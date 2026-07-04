b1eebbb963f55fd49b7b8daec8675439a637a20a
mark alexander <alexamu@mail.uc.edu>
2026-07-03 22:23:19 -0400

    m

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Random;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean done = false;
+        do {
+            Random generator = new Random();
+            int val = generator.nextInt(10) + 1;
+            int guess = 0;
+            System.out.println("Guess a number between 1 and 10: ");
+            if (scanner.hasNextInt()) {
+                guess = scanner.nextInt();
+                if (guess == val) {
+                    System.out.println("You guessed it!");
+                } else {
+                    System.out.println("Sorry, the number was " + val);
+                }
+            } else {
+                // consume the non-int token
+                System.out.println("Invalid input. Please enter a number next time.");
+                scanner.next();
+            }
+
+            System.out.println("Would you like to play again? (y/n)");
+            String resp = scanner.next();
+            if (resp.equalsIgnoreCase("n")) {
+                done = true;
+            }
+        } while (!done);
+        scanner.close();
+    }
 }
